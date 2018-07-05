4498d3278364039e84def52c635069c4cad50148
y1笑而过 <15335747148@163.com>
2018-07-05 10:18:50 +0800

    discriminator鉴别器
    discriminator鉴别器

---
+++ discriminator鉴别器/src/main/Main.java
@@ -0,0 +1,51 @@
+package main;
+
+import main.entities.AccountInfo;
+import main.mapper.AccountInfoMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigInteger;
+
+/**
+ * 每个基于 MyBatis 的应用都是以一个 SqlSessionFactory 的实例为中心的。
+ * SqlSessionFactory 的实例可以通过 SqlSessionFactoryBuilder 获得。
+ * 而 SqlSessionFactoryBuilder 则可以从 XML 配置文件或一个预先定制的 Configuration 的实例构建出 SqlSessionFactory 的实例。
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        InputStream in = null;
+        SqlSession session = null;
+        try {
+            // 读取配置文件
+            in = Resources.getResourceAsStream("mybatis-config.xml");
+            // 构建SqlSessionFactory
+            SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(in);
+            // 获取SqlSession
+            session = sessionFactory.openSession();
+
+            AccountInfoMapper accountInfoMapper = session.getMapper(AccountInfoMapper.class);
+            AccountInfo accountInfo = accountInfoMapper.getAccountInfoById(BigInteger.valueOf(1L));
+            System.out.println(accountInfo);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+}
