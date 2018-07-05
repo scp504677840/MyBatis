4bee74ef80b064fde092346730c262328128e900
y1笑而过 <15335747148@163.com>
2018-07-05 21:10:28 +0800

    缓存失效情况
    缓存失效情况

---
+++ 二级缓存/缓存失效情况/src/main/mapper/AccountInfoMapper.java
@@ -0,0 +1,12 @@
+package main.mapper;
+
+import main.entities.AccountInfo;
+import org.apache.ibatis.annotations.Param;
+
+import java.math.BigInteger;
+
+public interface AccountInfoMapper {
+
+    AccountInfo getAccountInfoById(@Param("id") BigInteger id);
+
+}
