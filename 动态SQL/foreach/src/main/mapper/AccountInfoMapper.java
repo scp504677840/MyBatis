fbfbcb069d4611415706c6123cbee32b539bfb7d
y1笑而过 <15335747148@163.com>
2018-07-05 18:16:04 +0800

    foreach
    foreach

---
+++ 动态SQL/foreach/src/main/mapper/AccountInfoMapper.java
@@ -0,0 +1,13 @@
+package main.mapper;
+
+import main.entities.AccountInfo;
+import org.apache.ibatis.annotations.Param;
+
+import java.math.BigInteger;
+import java.util.List;
+
+public interface AccountInfoMapper {
+
+    List<AccountInfo> getAccountInfoForeach(@Param("ids") List<BigInteger> ids);
+
+}
